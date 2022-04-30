2c7b28db205041507fc451d3941d75f29d742108
javagreet <javagreet@gmail.com>
2022-04-30 13:02:54 +0800

    master commit2

--- src/main/java/main/java/Hello.java
+++ src/main/java/main/java/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
         System.out.println("哈哈哈哈哈笑死我了！");
         System.out.println("33333333333333333");
         System.out.println("hot-fix申请出战！");
+        System.out.println("master test");
     }
 }
