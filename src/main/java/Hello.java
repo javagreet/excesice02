681c06cbf8afdcacae3fef2616a656ae1acb5e98
javagreet <javagreet@gmail.com>
2022-04-30 12:08:54 +0800

    idea second commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * @author Mr.Zheng
+ * @version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈笑死我了！");
+    }
+}
