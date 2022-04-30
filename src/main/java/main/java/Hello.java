efee3b707aeb756f8185e10123cfe8885dd47c26
javagreet <javagreet@gmail.com>
2022-04-30 12:56:11 +0800

    hot-fix commit1

--- src/main/java/main/java/Hello.java
+++ src/main/java/main/java/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("哈哈哈哈哈笑死我了！");
         System.out.println("33333333333333333");
+        System.out.println("hot-fix申请出战！");
     }
 }
